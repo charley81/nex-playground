7b156b3262f8a136b5b8e7829973f298797ab7c7
ChrisKnowsDev <chrisharley81@gmail.com>
2024-03-07 20:47:42 -0500

    delete functionality complete

--- components/tasks/delete-form.tsx
+++ components/tasks/delete-form.tsx
@@ -1,9 +1,23 @@
+'use client'
 import { Trash2 } from 'lucide-react'
+import { deleteTask } from '@/utils/actions'
+import { useFormStatus } from 'react-dom'
+import { Button } from '../ui/button'
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus()
+  return (
+    <button disabled={pending} className="flex items-center justify-center">
+      <Trash2 className="text-red-600 hover:cursor-pointer" />
+    </button>
+  )
+}
 
 export default function DeleteForm({ id }: { id: string }) {
   return (
-    <div>
-      <Trash2 className="text-red-600 hover:cursor-pointer" />
-    </div>
+    <form action={deleteTask}>
+      <input type="hidden" name="id" value={id} />
+      <SubmitButton />
+    </form>
   )
 }
--- utils/actions.tsx
+++ utils/actions.tsx
@@ -22,3 +22,12 @@ export const createTask = async (formData: FormData) => {
 
   revalidatePath('/tasks')
 }
+
+export const deleteTask = async (formData: FormData) => {
+  const id = formData.get('id') as string
+
+  await prisma.task.delete({
+    where: { id }
+  })
+  revalidatePath('/tasks')
+}
